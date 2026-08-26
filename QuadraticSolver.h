784a770ef0698a6148598bc932fe549b6b288c2a
NikitaNevezhin <nevezhin.nikity@gmail.com>
2026-08-26 18:11:26 +0300

    k,h

---
+++ QuadraticSolver.h
@@ -0,0 +1,5 @@
+void         SolveLinear              (struct Equation *equation_info);
+
+void         SolveQuadratic           (struct Equation *equation_info);
+
+double       CalcDiscriminant         (struct Coefficients *input_coeffs);
